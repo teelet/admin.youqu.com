72629fc27dbf0946472b323481ddb8575de9c2af
System Administrator <root@bj-m-14760a.local>
2016-08-29 19:15:26 +0800

    auth

---
+++ conf/auth.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * 权限配置文件
+ * shaohao
+ */
+
+return array(
+
+    '0001' => array(
+        'authName'  => '内容管理',
+        'routeName' => '内容管理'
+    ),
+    '0002' => array(
+        'authName'  => '更新知识库',
+        'routeName' => '其他操作 > 更新知识库'
+    ),
+    '0003' => array(
+        'authName'  => '添加内容',
+        'routeName' => '其他操作 > 添加内容'
+    ),
+    '0004' => array(
+        'authName'  => '添加标签',
+        'routeName' => '其他操作 > 添加标签'
+    ),
+    '0005' => array(
+        'authName'  => '添加别名',
+        'routeName' => '其他操作 > 添加别名'
+    ),
+    '0006' => array(
+        'authName'  => '权限管理',
+        'routeName' => '权限管理'
+    ),
+);
+
